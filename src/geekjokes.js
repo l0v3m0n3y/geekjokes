8076812c2f57911220353344cdd93e7e587b53e9
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-10-29 01:23:26 +0300

    refactor: move geekjokes.js to src directory

---
+++ src/geekjokes.js
@@ -0,0 +1,23 @@
+class geekjokes{
+    constructor(){
+        this.api = "https://geek-jokes.sameerkumar.website"
+        this.headers={"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/73.0.3683.86 Chrome/73.0.3683.86 Safari/537.36","x-requested-with": "xmlhttprequest"}
+    }
+    
+    async req(url){
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'GET',
+                headers: this.headers,
+            }).then(res => res.json()).then(data => {resolve(data);
+            }).catch(err => reject(err));
+        });
+    }
+    async get_joke(){
+        return (await this.req(`${this.api}/api?format=json`));
+    }
+    async get_info(){
+        return (await this.req(`${this.api}/`));
+    }
+}
+module.exports = {geekjokes};
